b5d15c861251e173178c3e370aafb2fb67c1c8c9
jeehk <jeehyunk86@daum.net>
2019-10-04 13:20:24 +0900

    사용자 정보 ms

---
+++ user/src/main/java/don/user/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package don.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import don.user.domain.DonUserInfo;
+import don.user.mapper.UserRepository;
+
+@Service
+@Slf4j
+class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepo; //repository
+	
+	public boolean confirmUserInfo(String userLoginId, String userPassword) {
+		
+		boolean result = false;
+		
+		try
+		{			
+			DonUserInfo resultData = userRepo.selectUserInfo(userLoginId, userPassword);
+			int id = resultData.getUserId();
+			
+			if(id > 0) {
+				result = true;
+			}			
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			result = false;
+		
+		} finally {
+			// TODO: handle finally clause
+			return result;
+
+		}
+
+	}
+
+	
+	
+	
+}
